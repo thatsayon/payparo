e35d58269e539e5763bba6d758d2f2a6c1cc2896
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2026-04-16 23:26:12 +0600

    smtp bug fixed

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -12,7 +12,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST', default='smtp.hostinger.com')
 EMAIL_PORT = env.int('EMAIL_PORT', default=465)
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=False)
-EMAIL_USE_SSL = env.bool('EMAIL_USE_TLS', default=True)
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', default=True)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='no-reply@ovation-app.com')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='OvationAPP123!')
 
